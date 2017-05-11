f8e5225e71abbb07c36f3a6249840bc70c4a3a3b
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-05-11 16:31:48 -0400

    add in heroku dyno count as a plugin

---
+++ .instrumental_scripts/heroku_dyno_count.sh
@@ -0,0 +1,9 @@
+curl -s https://api.heroku.com/apps/$APP_NAME_TO_COUNT_DYNOS/dynos -H "Accept: application/vnd.heroku+json; version=3"  -H "Authorization: Bearer $API_KEY_FOR_HEROKU" |
+grep type                            | # look for the line in each response that looks like: "type": "web",
+cut -d ':' -f 2                      | # take only "web",
+sed s/\"//g                          | # remove the quotation marks
+sed s/,//g                           | # remove the comma
+xargs -L 1 echo heroku.dyno-count.$1 | # echo an identifier for each, i.e., heroku.dyno.web.count
+sed 's/ //g'                         | # remove any remaining whitespace (not sure why it's there)
+uniq -c                              | # get a count of each unique identifier eg. ( 3 heroku.dyno.web.count \n 2 heroku.dyno.sidekiq.count )
+awk '{print $2,$1}'                    # switch them around so it's identifier then count to meet instrumental's requirements
