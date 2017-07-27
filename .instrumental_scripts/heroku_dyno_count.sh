c3225004c2f0dd9ef8740e95c0803319cf8e7d64
Jacob Smith <jacob.wesley.smith@gmail.com>
2017-07-27 10:05:34 -0400

    comment out heroky-dyno-count

--- .instrumental_scripts/heroku_dyno_count.sh
+++ .instrumental_scripts/heroku_dyno_count.sh
@@ -1,7 +1,7 @@
-curl -s https://api.heroku.com/apps/$APP_NAME_TO_COUNT_DYNOS/dynos -H "Accept: application/vnd.heroku+json; version=3"  -H "Authorization: Bearer $API_KEY_FOR_HEROKU" |
-grep type                            | # look for the line in each response that looks like: "type": "web",
-cut -d ':' -f 2                      | # take only "web",
-sed s/\"//g                          | # remove the quotation marks
-sed s/,//g                           | # remove the comma
-uniq -c                              | # get a count of each unique identifier eg. ( 3 heroku.dyno.web.count \n 2 heroku.dyno.sidekiq.count )
-ruby -e 'puts STDIN.read.split("\n").map { |line| line.split(" ").reverse.join(" ") }' # split them, reverse them
+# curl -s https://api.heroku.com/apps/$APP_NAME_TO_COUNT_DYNOS/dynos -H "Accept: application/vnd.heroku+json; version=3"  -H "Authorization: Bearer $API_KEY_FOR_HEROKU" |
+# grep type                            | # look for the line in each response that looks like: "type": "web",
+# cut -d ':' -f 2                      | # take only "web",
+# sed s/\"//g                          | # remove the quotation marks
+# sed s/,//g                           | # remove the comma
+# uniq -c                              | # get a count of each unique identifier eg. ( 3 heroku.dyno.web.count \n 2 heroku.dyno.sidekiq.count )
+# ruby -e 'puts STDIN.read.split("\n").map { |line| line.split(" ").reverse.join(" ") }' # split them, reverse them
